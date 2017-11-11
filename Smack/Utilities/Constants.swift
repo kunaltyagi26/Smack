b73077c808c9c53f0d5b5853566bd77c00cdc6f8
kunaltyagi26 <kunaltyagi32@gmail.com>
2017-11-11 11:59:55 +0530

    Updated files

---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Kunal Tyagi on 11/11/17.
+//  Copyright © 2017 Kunal Tyagi. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
